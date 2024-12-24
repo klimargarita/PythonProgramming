588c57a00cee6f278bd4741fca0c0b33fa7898a2
klimargarita <klimova.margarita.04@gmail.com>
2024-12-24 19:38:47 +0300

    Update task_2.py
    Добавила в решение list comprehension

--- Лабораторная работа 4/task_2.py
+++ Лабораторная работа 4/task_2.py
@@ -1,14 +1,11 @@
 import json
 
 def task() -> float:
-    total_sum = 0.0
 
     with open('input.json', 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
-        for entry in data:
-            score = entry.get("score", 0)
-            weight = entry.get("weight", 0)
-            total_sum += score * weight
+        scores = [entry.get("score", 0) * entry.get("weight", 0) for entry in data]
+        total_sum = sum(scores)
     return round(total_sum, 3)
 
 print(task())
